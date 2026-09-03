fe20efed7cd2699e81708cbea22eadb78211c4b0
Raphael Lameira <raphael@exemplo.com>
2026-09-03 16:15:33 -0300

    feat: add user create and encryption password

--- src/route.js
+++ src/route.js
@@ -1,17 +1,9 @@
 import {Router} from "express";
-import User from './app/models/User.js';
+import UserController from "./app/controllers/UserController.js";
+
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-    const user = await User.create({
-        name: "Raphael",
-        email: "raphael@example.com",
-        password_hash: "123",
-
-    });
-        
-    return res.json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
