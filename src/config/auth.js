a7b9ca26d2c4dbbb2126f13e30e0e1dad7c356ea
Raphael Lameira <raphael@exemplo.com>
2026-09-09 15:19:50 -0300

    feat/add-verifications-and-middlewares

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'ac0ddf9e65d57b6a56b2453386cd5db5',
+    expiresIn: '7d',
+};
--- src/route.js
+++ src/route.js
@@ -1,9 +1,19 @@
 import {Router} from "express";
+
+import authMiddleware from "./app/middlewares/auth.js";
+
 import UserController from "./app/controllers/UserController.js";
+import SessionController from "./app/controllers/SessionController.js";
 
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
+
+
+
+routes.use(authMiddleware);
+routes.put("/users", UserController.update);
 
 export default routes;
