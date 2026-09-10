22661512dfe6b5ab3eef97973b8a3c52ca1381ff
Raphael Lameira <raphael@exemplo.com>
2026-09-10 15:41:39 -0300

    feat/add task-logic

---
+++ src/app/controllers/TaskController.js
@@ -0,0 +1,62 @@
+import Task from "../models/Task";
+import * as Yup from "yup";
+
+
+class TaskController {
+    
+    async index(req, res) {
+        const tasks = await Task.findAll({
+            where: { user_id: req.userId, check: false },
+            attributes: ['id', 'task', 'check'],
+        });
+        return res.json(tasks);
+    }
+    async update(req, res) {
+        const {id} = req.params;
+
+        const task = await Task.findByPk(id);
+        if (!task) {
+            return res.status(404).json({ error: "Task not found" });
+        }
+        await task.update(req.body);
+        return res.json(task);
+
+    }
+    async delete(req, res) {
+        const {id} = req.params;
+        const task = await Task.findByPk(id);
+        if (!task) {
+            return res.status(404).json({ error: "Task not found" });
+        }
+        if (task.user_id !== req.userId) {
+            return res.status(401).json({ error: "You don't have permission to delete this task" });
+        }
+        await task.destroy();
+        return res.send();
+    }
+    
+    
+    
+    async store(req, res) {
+
+        const schema = Yup.object().shape({
+            task: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: "Validation fails" });
+        }
+        const{task} = req.body;
+        
+
+        const tasks = await Task.create({
+          user_id: req.userId,
+          task,
+        });
+   
+
+        return res.json(tasks);
+
+    }
+}
+export default new TaskController();
--- src/route.js
+++ src/route.js
@@ -4,6 +4,7 @@ import authMiddleware from "./app/middlewares/auth.js";
 
 import UserController from "./app/controllers/UserController.js";
 import SessionController from "./app/controllers/SessionController.js";
+import TaskController from "./app/controllers/TaskController.js";
 
 
 const routes = new Router();
@@ -16,4 +17,9 @@ routes.post("/sessions", SessionController.store);
 routes.use(authMiddleware);
 routes.put("/users", UserController.update);
 
+
+routes.post("/tasks", TaskController.store);
+routes.get("/tasks", TaskController.index);
+routes.put("/tasks/:id", TaskController.update);
+routes.delete("/tasks/:id", TaskController.delete);
 export default routes;
